b7a010ba310d4c6afa4793f71cce44b931d5f108
Francis <francisbernal14@gmail.com>
2023-02-16 01:32:34 +0100

    set up default <App.js> component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    // add methods/hooks here
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
 }
 
 export default App;
