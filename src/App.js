1dd10f77c18f0f0930cf7d74cab2b601044a0cf0
Francis <francisbernal14@gmail.com>
2023-02-17 04:54:07 +0100

    create handleKeyPress function in <App /> component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,43 @@ import React from 'react';
 import './App.css';
 import * as components from './components/export';
 
+const pressedStyle = {
+    boxShadow: "none",
+    transform: "translate(2px, 2px) scale(0.9)"
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            display: ""
+            display: "",
+            isPressed: false
         }
         this.handleClick = this.handleClick.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     // add methods/hooks here
+    componentDidMount() {
+        document.addEventListener("keydown", this.handleKeyPress)
+    }
+
+    componentWillUnmount() {
+        document.addEventListener("keydown", this.handleKeyPress)
+    }
+    handleKeyPress(event) {
+        const letter = event.key.toUpperCase();
+        const audio = document.getElementById(letter);
+        if (audio != null) {
+            const sample = audio.parentElement.id;
+            this.setState({
+                display: sample
+            })
+            audio.currentTime = 0;
+            audio.play()
+        }
+    }
+
     handleClick(event) {
         // set sample name as <display> state to show off itself when its playing
         const sample = event.target.id;
